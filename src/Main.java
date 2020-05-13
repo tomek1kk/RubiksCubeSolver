549f7d033b924af0004aaaaf5dcfe2ca8ad4c9d3
tomek1kk <tomek1kkgw@gmail.com>
2020-05-13 18:36:13 +0200

    Added initial project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.opencv.core.Core;
+
+public class Main {
+    static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(Core.VERSION);
+    }
+}
